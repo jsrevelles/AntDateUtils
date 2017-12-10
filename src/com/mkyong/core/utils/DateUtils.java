30efa00c97bf9d5b4f5411271c637e82f8e0474f
jsalinas <jsalinas@ematiz.com>
2017-12-10 18:42:17 +0100

    Modificación java 2

--- src/com/mkyong/core/utils/DateUtils.java
+++ src/com/mkyong/core/utils/DateUtils.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class DateUtils {
 
 	public static void main(String[] args) {
+		System.out.println("Otro cambio");
 		System.out.println("Hola a todos");
 		System.out.println(getLocalCurrentDate());
 		
